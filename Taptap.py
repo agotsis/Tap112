37a83b9be90938b719a7e5f22f9c3e7c74bf7b97
seongkwh <seongkwh@andrew.cmu.edu>
2016-04-03 00:12:13 +0900

    Added keys

--- Taptap.py
+++ Taptap.py
@@ -1,22 +1,83 @@
 from __future__ import division,print_function
 from visual import *
+from PIL import Image
+from PIL import _imaging
+
+
+#http://vpython.org/contents/docs/materials.html
 
 zLength = 50
 border = 0
 scene = display(title='Examples of Tetrahedrons',
      x=0, y=0, width=800, height=600,
      center=(0,0,0), background=(0,0,0))
+name = "MarioBG"
+im = Image.open(name + ".jpg")
+im = im.resize((512,512),Image.ANTIALIAS)
+materials.saveTGA(name,im)
+data = materials.loadTGA(name)
+mariobg = materials.texture(data = data, mapping = "rectangular", interpolate = False, mipmap = False)
+#box(pos = (0,0,-2000),size = (5000,3000,1),material = materials.bricks)
+
 sphere(pos = (0,0,-zLength),radius = 1, color = color.red)
-cylinder(pos = (-21,0,zLength+border),axis = (-2,0,-zLength*10))
-cylinder(pos = (21,0,zLength+border),axis = (2,0,-zLength*10))
-box(pos = (0,0,zLength+border),length = 50,height = 1,width = 1)
+cylinder(pos = (-21,0,zLength),axis = (-2,0,-zLength*10))
+cylinder(pos = (21,0,zLength),axis = (2,0,-zLength*10))
+#box(pos = (0,0,zLength),length = 50,height = 1,width = 1)
+#box(pos = (0,0,zLength-15),length = 40,height = 1,width = 1)
 
-noteScale = dict("C": -17.5,"D":-12.5,"E":-7.5,"F":-2.5,"G":2.5,"A":7.5,"B":12.5,"C":17.5]
+w = 21
+width = 10
+#keyboard = box(pos=(0,0,zLength),size=(10,1,1),axis=(1,0,0),color = color.white)
+noteScale = {"C": -17.5,"D":-12.5,"E":-7.5,"F":-2.5,"G":2.5,"A":7.5,"B":12.5,"C1":17.5}
+def drawKeyBoard():
+    global noteScale
+    global zLength
+    scale = 6
+    index = 0
+    c = color.white
+    for note in noteScale:
+        if(c == color.white):
+            c = color.blue
+        else:
+            c = color.white
 
-def generateNote(note):
-    t1 = sphere(pos=(noteScale[note],0,-zLength*5),radius=1,color=color.green)
-    t1.visible = True
-    notes.add(t1)
+        index += 1
+        x = noteScale[note]
+        t1 = box(pos=(x,-1,zLength-7.5),size = (1*scale,0.3*scale,2*scale),radius=1,color=c)
+        t1.visible = True
+
+"""
+    global w
+    global width
+    wKeya = box(pos=(-4*width,-5,0),size=(1,0.3,2),color=color.white)
+    wKeys = box(pos=(-3*width,-5,0),size=(1,0.3,2),color=color.blue)
+    wKeyd = box(pos=(-2*width,-5,0),size=(1,0.3,2),color=color.white)
+    wKeyf = box(pos=(-1*width,-5,0),size=(1,0.3,2),color=color.blue)
+    wKeyj = box(pos=(0*width,-5,0),size=(1,0.3,2),color=color.white)
+    wKeyk = box(pos=(1*width,-5,0),size=(1,0.3,2),color=color.blue)
+    wKeyl = box(pos=(2*width,-5,0),size=(1,0.3,2),color=color.white)
+    wKey_ = box(pos=(3*width,-5,0),size=(1,0.3,2),color=color.blue) """  
+drawKeyBoard()
+scene.lights =[distant_light(direction=(0.5,0.5,0.2))]
+
+pressedA = False
+pressedS = False
+pressedD = False
+pressedF = False
+pressedJ = False
+pressedK = False
+pessedL = False
+pressed_ = False
+
+
+def generateNote():
+    global note
+    if(len(note) != 0):
+        x = noteScale[note]
+        t1 = sphere(pos=(x,0,-zLength*5),radius=1,color=color.green)
+        t1.visible = True
+        notes.add(t1)
+        note = ""
 
 def checkBorderLine(notes):
     global zLength
@@ -33,13 +94,18 @@ def moveNotes(notes,vel,dt):
             note.pos.z += vel * dt
 
 def keyPressed(evt):
-    global pause
-    if (evt.key == "a"):
-        
-
+    global note
+    if (evt.key == "a" or evt.key =="A"): note = "C"
+    elif (evt.key == "s" or evt.key =="S"): note = "D"
+    elif (evt.key == "d" or evt.key =="D"): note = "E"
+    elif (evt.key == "f" or evt.key =="F"): note = "F"
+    elif (evt.key == "j" or evt.key =="J"): note = "G"
+    elif (evt.key == "k" or evt.key =="K"): note = "A"
+    elif (evt.key == "l" or evt.key =="L"): note = "B"
+    elif (evt.key == ";"): note = "C1"    
 scene.autoscale=False
 scene.userzoom = False
-scene.userspin = False
+scene.userspin = True
 scene.range = zLength
 
 myFrame = frame()
@@ -47,7 +113,7 @@ scene.forward = scene.forward.rotate(angle=-0.2, axis=(1,0,0))
 #myFrame.rotate(axis = (0,0,1), angle = 1.5682524)
 
 notes = set()
-
+note = ""
 zVelocity=150
 t=0
 dt=0.001
@@ -62,11 +128,13 @@ while advance:
     if not pause:
         moveNotes(notes,zVelocity,dt)
         checkBorderLine(notes)
+        generateNote()
         #print(diff_angle((1,0,0),scene.forward))
         if(len(notes) ==0):
-            generateNotes()
+            #generateNote(note)
             #pause = True
             #print("DONE")
+            pass
         t += dt
     #zBound = scene.mouse.camera.z - (2/tan(scene.fov/2))
     """
@@ -75,3 +143,12 @@ while advance:
             obj.visible = False
             del obj
         advance = not advance"""
+
+"""
+http://www.twitrcovers.com/wp-content/uploads/2014/02/Super-Mario-l.jpg
+
+
+"""
+
+
+
