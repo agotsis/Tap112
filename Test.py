eaedecc8c0a44c0a9b092615d209d4f2c7f28d5f
woffett <jonathanli.chocolate@gmail.com>
2016-04-02 02:09:11 -0400

    Tested playtaptap

--- Test.py
+++ Test.py
@@ -1,10 +1,18 @@
-import scipy.io.wavfile as wavfile
-from BasicFFT import basicFFT, intervalFFT
+import thread
+import time
+from fileFFT import fileFFT
+from PlaySound import AudioFile
 
-rate,data = wavfile.read("bin/mixedTest.wav")
-
-print(intervalFFT(rate,data,250))
-
-rate,data = wavfile.read("bin/420+700Hz.wav")
-
-print(basicFFT(rate,data))
+def playTaptap(filename):
+    freqList = fileFFT(filename)
+    a = AudioFile(filename)
+    def playSound(a):
+        #timerdelay here
+        a.play()
+        #timerdelay here
+        a.close()
+    try:
+        thread.start_new_thread(VPythonfunction,(freqList))
+        thread.start_new_thread(playSound(a))
+    except:
+        print("Error: unable to start thread!")
