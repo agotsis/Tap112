b34d9bef5bae2f9a2e7c2822f686530d47e7da7c
agotsis <alexgotsis8@gmail.com>
2016-04-02 01:11:33 -0400

    Updated intervalFFT

--- BasicFFT.py
+++ BasicFFT.py
@@ -1,14 +1,27 @@
+from __future__ import division, print_function
 import numpy as np
 import scipy.fftpack
 
-def basicFFT(rate,arrayData):
-    #takes in data from wavfile.read("fileName.wav")
-    N = len(arrayData)
+def basicFFT(rate, dataArray):
+    N = len(dataArray)
     freqSpacing = rate/N
-    rawFFT = scipy.fftpack.fft(arrayData)
+    rawFFT = scipy.fftpack.fft(dataArray)
     positiveFreqs = np.abs(rawFFT[:N//2])
     maxAmplitude = np.amax(positiveFreqs,axis=0)
     epsilon = 0.05*maxAmplitude
     peakIndex = np.argwhere(abs(positiveFreqs-maxAmplitude)<epsilon)
     freq = freqSpacing*peakIndex
     return freq.flatten()
+
+def intervalFFT(rate, dataArray, msInterval):
+    samplesPerInterval = int(msInterval / 1000 * rate)
+    N = len(dataArray)
+
+    intervals = []
+    for startIndex in range(0, N - samplesPerInterval, samplesPerInterval):
+        intervals.append(dataArray[startIndex:startIndex+samplesPerInterval])
+
+    frequencies = []
+    for interval in intervals:
+        frequencies.append(basicFFT(rate, interval))
+    return frequencies
--- Test.py
+++ Test.py
@@ -1,5 +1,9 @@
 import scipy.io.wavfile as wavfile
-from BasicFFT import basicFFT
+from BasicFFT import basicFFT, intervalFFT
+
+rate,data = wavfile.read("bin/mixedTest.wav")
+
+print(intervalFFT(rate,data,250))
 
 rate,data = wavfile.read("bin/420+700Hz.wav")
 
